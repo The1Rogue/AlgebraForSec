2a713628c4ffe98b6a67a32776bcf94eb5037a38
evgenikrustev1 <67074464+evgenikrastev1@users.noreply.github.com>
2021-09-21 23:18:01 +0200

    Update Main.py
    Structured the file with functions for every operation and added the given exercises, starting from Jelmers already added code from 20 sept

--- Main.py
+++ Main.py
@@ -9,31 +9,164 @@ ops_loc = base_location + 'operations.asn'
 exs_loc = base_location + 'my_exercises'
 ans_loc = base_location + 'my_answers'
 
+# global counter for the number of elementary additions/subtractions operations
+count_add = 0
+# global counter for the number of elementary multiplication operations
+count_mul = 0
+
+# global variables for result of Euclidean Algorithm
+answ_d = ''
+answ_a = ''
+answ_b = ''
+
 ###### Creating an exercise list file ######
 
-# How to create an exercise JSON file containing one addition exercise
-exercises = {'exercises' : []}                                     # initialize empty exercise list
-ex = {'add' : {'radix' : 10, 'x' : '3', 'y' : '4', 'answer' : ''}} # create add exercise
-exercises['exercises'].append(ex)                                  # add exercise to list
+# Create a JSON file containing exercises
+def createExerciseJSONfile():
+    exercises = {'exercises' : []}                                      # initialize empty exercise list
 
-# Encode exercise list and print to file
-my_file = open(exs_loc, 'wb+')                                     # write to binary file
-my_file.write(json.dumps(exercises).encode())                      # add encoded exercise list
-my_file.close()
+    # example exercise
+    ex = {'add' : {'radix' : 10, 'x' : '-150', 'y' : '-6', 'answer' : ''}} # create add exercise
+    exercises['exercises'].append(ex)                                   # add exercise to list
+
+
+    # given exercises
+
+    ex = {'add' : {'radix' : 2, 'x' : '1010100001110101001010001100101110111101101110000101011100111000000000010111010010110000001101011100', 'y' : '1001100111111111001111101100000011100001111100100001001001011000101111111010011111010011010101000110', 'answer' : '10100001001110100011001111000110010011111101010100110100110010000110000010001110010000011100010100010'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'add' : {'radix' : 2, 'x' : '1010100001110101001010001100101110111101101110000101011100111000000000010111010010110000001101011100', 'y' : '-1001100111111111001111101100000011100001111100100001001001011000101111111010011111010011010101000110', 'answer' : '111001110101111010100000101011011011110001100100010011011111010000011100110011011100111000010110'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'subtract' : {'radix' : 2, 'x' : '1010100001110101001010001100101110111101101110000101011100111000000000010111010010110000001101011100', 'y' : '1001100111111111001111101100000011100001111100100001001001011000101111111010011111010011010101000110', 'answer' : '111001110101111010100000101011011011110001100100010011011111010000011100110011011100111000010110'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'subtract' : {'radix' : 2, 'x' : '1010100001110101001010001100101110111101101110000101011100111000000000010111010010110000001101011100', 'y' : '-1001100111111111001111101100000011100001111100100001001001011000101111111010011111010011010101000110', 'answer' : '10100001001110100011001111000110010011111101010100110100110010000110000010001110010000011100010100010'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'multiply' : {'radix' : 2, 'x' : '1010100001110101001010001100101110111101101110000101011100111000000000010111010010110000001101011100', 'y' : '1001100111111111001111101100000011100001111100100001001001011000101111111010011111010011010101000110', 'answer' : '1100101010101011111101101100000101100111100001101011011001011101000111010100011101110100001111110001110111001101101001100110001110010110011001111000100110110011011010101100010001000001111011100101000', 'count-mul': '...', 'count-add': '...'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'karatsuba' : {'radix' : 2, 'x' : '1010100001110101001010001100101110111101101110000101011100111000000000010111010010110000001101011100', 'y' : '1001100111111111001111101100000011100001111100100001001001011000101111111010011111010011010101000110', 'answer' : '1100101010101011111101101100000101100111100001101011011001011101000111010100011101110100001111110001110111001101101001100110001110010110011001111000100110110011011010101100010001000001111011100101000', 'count-mul': '...', 'count-add': '...'}}
+    #exercises['exercises'].append(ex)
+
+
+    ex = {'add' : {'radix' : 3, 'x' : '2020111212202021100001212221022010021001', 'y' : '1202222101212012211022202222112120020101', 'answer' : '11000111021121111011101122220211200111102'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'add' : {'radix' : 3, 'x' : '2020111212202021100001212221022010021001', 'y' : '-1202222101212012211022202222112120020101', 'answer' : '110112110220001111202002221202120000200'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'subtract' : {'radix' : 3, 'x' : '2020111212202021100001212221022010021001', 'y' : '1202222101212012211022202222112120020101', 'answer' : '110112110220001111202002221202120000200'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'subtract' : {'radix' : 3, 'x' : '2020111212202021100001212221022010021001', 'y' : '-1202222101212012211022202222112120020101', 'answer' : '11000111021121111011101122220211200111102'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'multiply' : {'radix' : 3, 'x' : '2020111212202021100001212221022010021001', 'y' : '1202222101212012211022202222112120020101', 'answer' : '10222112100001022222012101100012020101212101011201011110022221012121011022211101', 'count-mul': '...', 'count-add': '...'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'karatsuba' : {'radix' : 3, 'x' : '2020111212202021100001212221022010021001', 'y' : '1202222101212012211022202222112120020101', 'answer' : '10222112100001022222012101100012020101212101011201011110022221012121011022211101', 'count-mul': '...', 'count-add': '...'}}
+    #exercises['exercises'].append(ex)
+
+
+    ex = {'add' : {'radix' : 7, 'x' : '255250226011363161263502222556', 'y' : '153556502550413362430240350510', 'answer' : '442140031562106554024042603366'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'add' : {'radix' : 7, 'x' : '255250226011363161263502222556', 'y' : '-153556502550413362430240350510', 'answer' : '101360423130646465533231542046'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'subtract' : {'radix' : 7, 'x' : '255250226011363161263502222556', 'y' : '153556502550413362430240350510', 'answer' : '101360423130646465533231542046'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'subtract' : {'radix' : 7, 'x' : '255250226011363161263502222556', 'y' : '-153556502550413362430240350510', 'answer' : '442140031562106554024042603366'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'multiply' : {'radix' : 7, 'x' : '255250226011363161263502222556', 'y' : '153556502550413362430240350510', 'answer' : '50303335051551042446540232630332141424231165632644465060060', 'count-mul': '...', 'count-add': '...'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'karatsuba' : {'radix' : 7, 'x' : '255250226011363161263502222556', 'y' : '153556502550413362430240350510', 'answer' : '50303335051551042446540232630332141424231165632644465060060', 'count-mul': '...', 'count-add': '...'}}
+    #exercises['exercises'].append(ex)
+
+
+    ex = {'add' : {'radix' : 16, 'x' : '82a4d3f8bfab54bb3011', 'y' : 'cb95aa820d14888e48c3', 'answer' : '14e3a7e7accbfdd4978d4'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'add' : {'radix' : 16, 'x' : '82a4d3f8bfab54bb3011', 'y' : '-cb95aa820d14888e48c3', 'answer' : '-48f0d6894d6933d318b2'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'subtract' : {'radix' : 16, 'x' : '82a4d3f8bfab54bb3011', 'y' : 'cb95aa820d14888e48c3', 'answer' : '-48f0d6894d6933d318b2'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'subtract' : {'radix' : 16, 'x' : '82a4d3f8bfab54bb3011', 'y' : '-cb95aa820d14888e48c3', 'answer' : '14e3a7e7accbfdd4978d4'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'multiply' : {'radix' : 16, 'x' : '82a4d3f8bfab54bb3011', 'y' : 'cb95aa820d14888e48c3', 'answer' : '67e5150972e817d639ac0f8795213f07e18864f3', 'count-mul': '...', 'count-add': '...'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'karatsuba' : {'radix' : 16, 'x' : '82a4d3f8bfab54bb3011', 'y' : 'cb95aa820d14888e48c3', 'answer' : '67e5150972e817d639ac0f8795213f07e18864f3', 'count-mul': '...', 'count-add': '...'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'reduce' : {'radix' : 16, 'x' : 'd26936c465648ef03a1ade904737b30428155781', 'm' : '157f77a46f4c796bb774', 'answer' : '2c0793480f2afd6dc5d'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'mod-add' : {'radix' : 16, 'x' : '93f76ca85dfdbf3f1790', 'y' : 'c2a72e55e1956be991ca', 'm' : '157f77a46f4c796bb774', 'answer' : '1426985bba180dd8e98e'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'mod-subtract' : {'radix' : 16, 'x' : '93f76ca85dfdbf3f1790', 'y' : 'c2a72e55e1956be991ca', 'm' : '157f77a46f4c796bb774', 'answer' : '11cea53fca4dbf98ac22'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'mod-subtract' : {'radix' : 16, 'x' : 'c2a72e55e1956be991ca', 'y' : '93f76ca85dfdbf3f1790', 'm' : '157f77a46f4c796bb774', 'answer' : '3b0d264a4feb9d30b52'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'mod-multiply' : {'radix' : 16, 'x' : '93f76ca85dfdbf3f1790', 'y' : 'c2a72e55e1956be991ca', 'm' : '157f77a46f4c796bb774', 'answer' : 'dad2e63149941a790c4'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'euclid' : {'radix' : 16, 'x' : 'c1b715933d2d1dcb0e23', 'y' : '157f77a46f4c796bb774', 'answ-d' : '1', 'answ-a' : '8bb87443ec917fa3e87', 'answ-b' : '-4eb01402d28cbe3588c1'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'inverse' : {'radix' : 16, 'x' : 'c1b715933d2d1dcb0e23', 'm' : '157f77a46f4c796bb774', 'answer' : '8bb87443ec917fa3e87'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'euclid' : {'radix' : 16, 'x' : 'b22b5d17e57a41599185', 'y' : '157f77a46f4c796bb774', 'answ-d' : '19', 'answ-a' : '-74ba5fd6968445267', 'answ-b' : '3c769a8d705995e753'}}
+    #exercises['exercises'].append(ex)
+
+    ex = {'inverse' : {'radix' : 16, 'x' : 'b22b5d17e57a41599185', 'm' : '157f77a46f4c796bb774', 'answer' : 'ERROR - inverse does not exist'}}
+    #exercises['exercises'].append(ex)
+
+    # Encode exercise list and print to file
+    my_file = open(exs_loc, 'wb+')                                     # write to binary file
+    my_file.write(json.dumps(exercises).encode())                      # add encoded exercise list
+    my_file.close()
+    return
+
+# call this function to create exercises-file with exercises included inside it
+# or put it in comment to use existing exercises-file
+createExerciseJSONfile()
 
 ###### Using an exercise list file ######
 
 # Compile specification
 spec = asn.compile_files(ops_loc, codec = "jer")
 
-# Read exercise list 
+# Read exercise list
 exercise_file = open(exs_loc, 'rb')                                # open binary file
 file_data = exercise_file.read()                                   # read byte array
 my_exercises = spec.decode('Exercises', file_data)                 # decode after specification
-exercise_file.close()                                              
+exercise_file.close()
 
 # Create answer JSON
 my_answers = {'exercises': []}
+
+def padArray(x, y):
+  if (len(x) != len(y)):
+    for i in range(max(len(x), len(y))+1):
+      if len(x) < i:
+        x.insert(1, 0)
+      if len(y) < i:
+        y.insert(1, 0)
+  return x, y
+
 
 #turns an input into a list, to use in operations
 #input s:string example: "-1f34"
@@ -59,70 +192,166 @@ def toString(l):
 
     out += "".join([map[i] for i in l[1:]])
     return out
+def substract(r, x, y):
+  answer = []
+  return answer
 
+
+def addition(r, x, y):
+  answer = []
+  carry = 0
+  signx = True if x[0] == "neg" else False
+  signy = True if y[0] == "neg" else False
+  if (signx and signy) or (not(signx) and not(signy)):
+    for i in range(len(x)-1, 0 ,-1):
+      a = x[i]
+      b = y[i]
+      if a+b+carry < r:
+        ins = a+b+carry
+        carry = 0
+      else:
+        ins = a+b+carry-r
+        carry = 1
+      answer.insert(0, ins)
+    if signx:
+      answer.insert(0, "neg")
+    else:
+      answer.insert(0, "pos")
+  else:
+    x[0] = 'pos'
+    y[0] = 'pos'
+    if signx:
+      answer = substract(r, y, x)
+    elif signy:
+      answer = substract(r, x, y)
+
+  return answer
+
+def modAdd(radix, x, y, m):
+    answer = ''
+    return answer
+
+def modSubtract(radix, x, y, m):
+    answer = ''
+    return answer
+
+def multiply(radix, x, y):
+    answer = ''
+    #ToDo: calculate count_mul
+    #ToDo: calculate count_add
+    return answer
+
+def modMultiply(radix, x, y, m):
+    answer = ''
+    return answer
+
+def karatsuba(radix, x, y):
+    answer = ''
+    #ToDo: calculate count_mul
+    #ToDo: calculate count_add
+    return answer
+
+def reduce(radix, x, m):
+    answer = ''
+    return answer
+
+def euclid(radix, x, y):
+    #ToDo: calculate answ_d
+    #ToDo: calculate answ_a
+    #ToDo: calculate answ_b
+    return
+
+def inverse(radix, x, m):
+    answer = ''
+    return answer
 
 
 # Loop over exercises and solve
 for exercise in my_exercises['exercises']:
-    operation = exercise[0]                                        # get operation type
-    params = exercise[1]                                           # get parameters
+    operation = exercise[0]                # get operation type
+
+    # clear global counters before each operation
+    count_mul = 0
+    count_add = 0
+
+    # get parameters
+    params = exercise[1]
+    if 'x' in params:
+        x = params['x']
+    else:
+        x = ''
+    if 'y' in params:
+        y = params['y']
+    else:
+        y = ''
+    if 'm' in params:
+        m = params['m']
+    else:
+        m = ''
+    if 'radix' in params:
+        radix = params['radix']
+    else:
+        radix = ''
 
     if operation == 'add':
-        ### Do addition ###
-        params['answer'] = '7'
-        
-    if operation == 'mod-add':
+      x, y = padArray(parseString(params["x"]), parseString(params["y"]))
+      ans = addition(radix, x, y)
+      print(ans)
+
+      # params['answer'] = toString(ans)
+
+    elif operation == 'mod-add':
         ### Do modular addition ###
-        params['answer'] = '1234'
-    
-    if operation == 'subtract':
+        params['answer'] = modAdd(radix, x, y, m)
+
+    elif operation == 'subtract':
         ### Do subtraction ###
-        params['answer'] = '-0'
-        
-    if operation == 'mod-substract':
+        params['answer'] = substract(radix, x, y)
+
+    elif operation == 'mod-subtract':
         ### Do euclidean algorithm ###
-        params['answ-d'] = '1'
-        params['answ-a'] = '0'
-        params['answ-b'] = '0'
-        
-    if operation == 'multiply':
+        params['answer'] = modSubtract(radix, x, y, m)
+
+    elif operation == 'multiply':
         ### Do multiplication ###
-        params['answer'] = '66'
-        params['count-mul'] = '1'
-        params['count-add'] = '2'
+        params['answer'] = multiply(radix, x, y)
+        # add calculated counters to the result
+        params['count-mul'] = count_mul
+        params['count-add'] = count_add
 
-    if operation == 'mod-multiply':
+    elif operation == 'mod-multiply':
         ### Do multiplication ###
-        params['answer'] = '66'
-        params['count-mul'] = '1'
-        params['count-add'] = '2'        
+        params['answer'] = modMultiply(radix, x, y, m)
 
-    if operation == 'karatsuba':
-        ### Do euclidean algorithm ###
-        params['answ-d'] = '1'
-        params['answ-a'] = '0'
-        params['answ-b'] = '0'
+    elif operation == 'karatsuba':
+        ### Do karatsuba algorithm ###
+        params['answer'] = karatsuba(radix, x, y)
+        # add calculated counters to the result
+        params['count-mul'] = count_mul
+        params['count-add'] = count_add
 
-    if operation == 'reduce':
-        ### Do euclidean algorithm ###
-        params['answ-d'] = '1'
-        params['answ-a'] = '0'
-        params['answ-b'] = '0'            
+    elif operation == 'reduce':
+        ### Do reduce algorithm ###
+        params['answer'] = reduce(radix, x, m)
 
-    if operation == 'euclid':
+    elif operation == 'euclid':
         ### Do euclidean algorithm ###
-        params['answ-d'] = '1'
-        params['answ-a'] = '0'
-        params['answ-b'] = '0' 
-        
-    if operation == 'inverse':
-        ### Do euclidean algorithm ###
-        params['answ-d'] = '1'
-        params['answ-a'] = '0'
-        params['answ-b'] = '0'      
+        euclid(radix, x, y)
+        # add calculated answers to the result
+        params['answ-d'] = answ_d
+        params['answ-a'] = answ_a
+        params['answ-b'] = answ_b
+
+    elif operation == 'inverse':
+        ### Do inverse algorithm ###
+        params['answer'] = inverse(radix, x, m)
+
+    else:
+        print("Invalid input: not supported operation in [", radix, "] = ", operation)
 
     # Save answer
     my_answers['exercises'].append({operation: params})
+
 
 ###### Creating an answers list file ######
 
